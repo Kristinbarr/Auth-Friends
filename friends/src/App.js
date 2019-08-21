19234901fab49260f0c650a4d1867c8d005d8463
Kristin Barr <kristinbarr@gmail.com>
2019-08-21 00:46:57 -0400

    added private route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Link from './components/Login'
+import Login from './components/Login'
+import FriendList from './components/FriendList'
+import PrivateRoute from './components/PrivateRoute'
+
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <Router>
-        <Link to='/login'>Login</Link>
-        <Route path='/login' component={Login} />
+        <div className='nav'>
+          <Link to='/login'>Login</Link>
+          <Link to='/protected'>Protected Page</Link>
+        </div>
+        <div className='content'>
+          <Route path='/login' component={Login} />
+          <PrivateRoute exact path='/protected' component={FriendList} />
+        </div>
       </Router>
     </div>
   )
