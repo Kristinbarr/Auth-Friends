d27fa97af574de798fc728b30eabcd33b3d13321
Kristin Barr <kristinbarr@gmail.com>
2020-05-13 16:17:03 -0400

    formatting

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -8,7 +8,7 @@ import FriendForm from './FriendForm'
 class FriendList extends React.Component {
   state = {
     friends: [],
-    fetchingData: false
+    fetchingData: false,
   }
 
   componentDidMount() {
@@ -18,47 +18,48 @@ class FriendList extends React.Component {
   getData = () => {
     axiosWithAuth()
       .get('http://localhost:5000/api/friends')
-      .then((res) => {
+      .then(res => {
         this.setState({
-          friends: res.data
+          friends: res.data,
         })
       })
-      .catch((err) => console.log(err.response))
-  }
-  deleteFriend = (id) => {
-    axiosWithAuth()
-      .delete(`http://localhost:5000/api/friends/${id}`)
-      .then((res) => {
-        this.setState({friends: res.data})
-      })
-      .catch((err) => console.log(err.response))
+      .catch(err => console.log(err.response))
   }
 
-  // formatData = () => {
-  //   const formattedData = []
-  // console.log('formatting', this.state.friends)
-  // this.state.friends.forEach((id, index, arr) => {
-  //   if (price.location === 'US') {
-  //     formattedData.push({
-  //       date: moment(price.date).format('MMM'),
-  //       USPrice: price.price,
-  //       HawaiiPrice: arr[index + 1].price
-  //     })
-  //   }
-  // })
-  //   return formattedData
-  // }
+  deleteFriend = id => {
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/friends/${id}`)
+      .then(res => {
+        this.setState({ friends: res.data })
+      })
+      .catch(err => console.log(err.response))
+  }
+
+  formatData = () => {
+    const formattedData = []
+    console.log('formatting', this.state.friends)
+    this.state.friends.forEach((id, index, arr) => {
+      if (price.location === 'US') {
+        formattedData.push({
+          date: moment(price.date).format('MMM'),
+          USPrice: price.price,
+          HawaiiPrice: arr[index + 1].price,
+        })
+      }
+    })
+    return formattedData
+  }
 
   render() {
     // const friends = this.formatData()
     return (
-      <div className='gas-prices'>
-        <div className='title-wrapper'>
-          <h2 className='top-title'>~*` FRIENDS `*~</h2>
-          <h3 className='bottom-title'>Add new friends here:</h3>
+      <div className="gas-prices">
+        <div className="title-wrapper">
+          <h2 className="top-title">~*` FRIENDS `*~</h2>
+          <h3 className="bottom-title">Add new friends here:</h3>
           <FriendForm />
         </div>
-        <div className=''>
+        <div className="">
           <h3>TOP 8 FRIENDS AND MORE</h3>
         </div>
         {/* {this.state.friends ? (
@@ -68,9 +69,9 @@ class FriendList extends React.Component {
           </div>
         ) :  */}
         {
-          <div className='friend-wrapper'>
-            {this.state.friends.map((friend) => (
-              <div className='friend' key={friend.email}>
+          <div className="friend-wrapper">
+            {this.state.friends.map(friend => (
+              <div className="friend" key={friend.email}>
                 <h4>{friend.name}</h4>
                 <p>age: {friend.age}</p>
                 <h6>email:</h6> <p>{friend.email}</p>
