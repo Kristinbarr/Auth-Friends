4898da322d15c10ad1bf80c72b358d61cc7e6aab
Kristin Barr <kristinbarr@gmail.com>
2020-05-24 01:12:36 -0400

    added switch to routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import Login from './components/Login'
 import FriendList from './components/FriendList'
 import PrivateRoute from './components/PrivateRoute'
@@ -8,16 +8,16 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
-        <div className='nav'>
-          <Link to='/login'>Login</Link>
-          <Link to='/protected'>Protected Page</Link>
+        <div className="nav">
+          <Link to="/login">Login</Link>
+          <Link to="/friends">Friends List</Link>
         </div>
-        <div className='content'>
-          <Route path='/login' component={Login} />
-          <PrivateRoute exact path='/protected' component={FriendList} />
-        </div>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/friends" component={FriendList} />
+        </Switch>
       </Router>
     </div>
   )
